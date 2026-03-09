d703c52f90be49de27a47564a7517123bff8aa18
PC_Clase <diegosouto2000@gmail.com>
2026-03-09 12:27:04 +0100

    Initial setup

--- Assets/Scripts/UI/XRInputKeyboard.cs
+++ Assets/Scripts/UI/XRInputKeyboard.cs
@@ -1,12 +1,50 @@
-using TMPro;
 using UnityEngine;
+using TMPro;
 
-public class XRInputKeyboard : MonoBehaviour
+public class QuestKeyboardHandler : MonoBehaviour
 {
-    public TMP_InputField input;
+    public TMP_InputField inputField;
 
-    public void OnSelect()
+#if UNITY_ANDROID && !UNITY_EDITOR
+    private TouchScreenKeyboard keyboard;
+#endif
+
+    private void Awake()
     {
-        TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+        if (inputField == null)
+            inputField = GetComponent<TMP_InputField>();
+
+        // Show keyboard when field is selected
+        inputField.onSelect.AddListener(OnInputSelected);
+    }
+
+    private void OnDestroy()
+    {
+        inputField.onSelect.RemoveListener(OnInputSelected);
+    }
+
+    private void OnInputSelected(string _)
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        // Open Meta Quest system keyboard
+        keyboard = TouchScreenKeyboard.Open(
+            inputField.text,
+            TouchScreenKeyboardType.Default,
+            false, // autocorrection
+            inputField.multiLine, // allow multiline if needed
+            false, // secure
+            false  // alert
+        );
+#endif
+    }
+
+    private void Update()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        if (keyboard != null && keyboard.active)
+        {
+            inputField.text = keyboard.text;
+        }
+#endif
     }
 }
